7142477e1f53b3afa208dc76de9ebb252cb54abc
Istallia <55042065+istallia@users.noreply.github.com>
2020-11-28 20:56:48 +0900

    [Add] ページ内とURLから素材情報を取得する処理を追加

--- content-script.js
+++ content-script.js
@@ -1,9 +1,11 @@
 /* --- 素材ページに来たらbackgroundにコモンズIDとタイトルを送信する --- */
 if (typeof browser === 'undefined') browser = chrome;
-const page_url = location.href.replace('https://').replace('http://');
+const page_url = location.href.replace('https://', '').replace('http://', '');
 if (page_url.startsWith('commons.nicovideo.jp/material') && !page_url.startsWith('commons.nicovideo.jp/material/agreement')) {
+	const material_title = document.querySelector('div.commons_title').innerText;
+	const material_id    = location.href.slice(29).replace('/', '');
 	const sending = browser.runtime.sendMessage({
-		material_id    : 'nc2525',
-		material_title : '素晴らしい素材'
+		material_id    : material_id,
+		material_title : material_title
 	});
 }
