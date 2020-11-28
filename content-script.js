3beb289a302297ea5a995831fbdad709319b7dd9
Istallia <55042065+istallia@users.noreply.github.com>
2020-11-28 20:46:49 +0900

    [Add] backgroundに送信する処理の追加

--- content-script.js
+++ content-script.js
@@ -1,1 +1,9 @@
 /* --- 素材ページに来たらbackgroundにコモンズIDとタイトルを送信する --- */
+if (typeof browser === 'undefined') browser = chrome;
+const page_url = location.href.replace('https://').replace('http://');
+if (page_url.startsWith('commons.nicovideo.jp/material') && !page_url.startsWith('commons.nicovideo.jp/material/agreement')) {
+	const sending = browser.runtime.sendMessage({
+		material_id    : 'nc2525',
+		material_title : '素晴らしい素材'
+	});
+}
