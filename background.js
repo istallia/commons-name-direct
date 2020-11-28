b3f1d00b73c2566c60671bdf543b6604f17b92b3
Istallia <55042065+istallia@users.noreply.github.com>
2020-11-28 20:09:42 +0900

    [Modify] 関数名の変更とヘッダ取得関数の追加

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 if (typeof browser === 'undefined') browser = chrome;
 browser.webRequest.onHeadersReceived.addListener(details => {
 	// refererを 'http://example.com/' に書き換え:
-	putReqHeader(details, 'Content-Disposition', 'attachment;');
+	setResponseHeader(details, 'Content-Disposition', 'attachment;');
 
 	return {
 		responseHeaders: details.responseHeaders,
@@ -13,8 +13,8 @@ browser.webRequest.onHeadersReceived.addListener(details => {
 	'blocking'
 ]);
 
-/** details.requestHeaders内のリクエストヘッダを更新する関数 */
-function putReqHeader(details, key, val) {
+/* details.requestHeaders内のレスポンスヘッダを更新する関数 */
+function setResponseHeader(details, key, val) {
 	key = key.toLowerCase();
 	for (let n in details.responseHeaders) {
 		const got = details.responseHeaders[n].name.toLowerCase() == key;
@@ -27,4 +27,16 @@ function putReqHeader(details, key, val) {
 		name: key,
 		value: val,
 	});
+}
+
+/* レスポンスヘッダを取得 */
+function getResponseHeader(details, key) {
+	key = key.toLowerCase();
+	for (let n in details.responseHeaders) {
+		const got = details.responseHeaders[n].name.toLowerCase() == key;
+		if (got) {
+			return details.responseHeaders[n].value;
+		}
+	}
+	return null;
 }
