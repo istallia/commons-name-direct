f5cae865bee0c585127b992fee5eadbaed86f7a6
Istallia <55042065+istallia@users.noreply.github.com>
2021-04-29 20:48:22 +0900

    [Fix] 素材名自動コピー機能がまだ動いてなかった

--- content-script.js
+++ content-script.js
@@ -4,8 +4,15 @@ const page_url = location.href.replace('https://', '').replace('http://', '');
 if (page_url.startsWith('commons.nicovideo.jp/material') && !page_url.startsWith('commons.nicovideo.jp/material/agreement')) {
 	/* backgroundにコモンズIDとタイトル、制作者を送信する */
 	let sendData = () => {
-		const material_title    = document.querySelector('div.materialHeadTitle').innerText;
-		const material_creator  = document.querySelector('div.mUserProfile a.materialUsername').innerText;
+		const element_title    = document.querySelector('div.materialHeadTitle');
+		const element_creator  = document.querySelector('div.mUserProfile a.materialUsername');
+		const element_button   = document.querySelector('a[href*="/material/agreement/"] > span');
+		if (element_title === null || element_creator === null || element_button === null) {
+			setTimeout(sendData, 200);
+			return;
+		}
+		const material_title    = element_title.innerText;
+		const material_creator  = element_creator.innerText;
 		const material_id       = page_url.slice(29).replace('/', '');
 		let sending_material_id = browser.runtime.sendMessage({
 			content          : 'material-id',
@@ -15,24 +22,21 @@ if (page_url.startsWith('commons.nicovideo.jp/material') && !page_url.startsWith
 		});
 		sessionStorage.setItem('commons-title-'+material_id, material_title);
 		sessionStorage.setItem('commons-creator-'+material_id, material_creator);
-	};
-	sendData();
-	document.querySelector('a[href*="/material/agreement/"] > span').addEventListener('click', sendData);
-	/* backgroundにオプションを問い合わせ、コピー機能がtrueならばボタンにイベントを登録する */
-	let asking_options = browser.runtime.sendMessage({content : 'get-option'}, options => {
-		if (options['copy-title']) {
-			let func = (pattern, event) => {
-				setTimeout(() => {
+		/* backgroundにオプションを問い合わせ、コピー機能がtrueならばボタンにイベントを登録する */
+		let asking_options = browser.runtime.sendMessage({content : 'get-option'}, options => {
+			if (options['copy-title']) {
+				let func = (pattern, event) => {
 					const material_id      = page_url.slice(29).replace('/', '');
 					const material_title   = sessionStorage.getItem('commons-title-'+material_id);
 					const material_creator = sessionStorage.getItem('commons-creator-'+material_id);
 					const copy_text        = pattern.replace('${id}', material_id).replace('${title}', material_title).replace('${creator}', material_creator);
 					navigator.clipboard.writeText(copy_text);
-				}, 0);
-			};
-			document.querySelector('a[href*="/material/agreement/"] > span').addEventListener('click', func.bind(this, options['title-pattern']));
-		}
-	});
+				};
+				element_button.addEventListener('click', func.bind(this, options['title-pattern']));
+			}
+		});
+	};
+	sendData();
 }
 
 
