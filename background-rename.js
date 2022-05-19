4627873a78830710e466710021a56d4b05a8d342
Istallia <55042065+istallia@users.noreply.github.com>
2022-05-19 21:46:26 +0900

    [Replace] 設定を保存していたlocalStorageをStorage APIに移行

--- background-rename.js
+++ background-rename.js
@@ -1,39 +1,45 @@
+
+if (typeof browser === 'undefined') browser = chrome;
+
 /* --- オプションの初期値を保存 --- */
-if (!localStorage.getItem('file_pattern')) {
-	localStorage.setItem('file_pattern', '${id}_${title}');
-	localStorage.setItem('copy_title', false);
-	localStorage.setItem('title_pattern', '${title} (${id})');
-}
+browser.storage.local.get(['file_pattern'], options => {
+	if (!options['file_pattern']) {
+		browser.storage.local.set({
+			file_pattern  : '${id}_${title}',
+			copy_title    : false,
+			title_pattern : '${title} (${id})'
+		});
+	}
+});
 
 
 /* --- ヘッダの書き換え --- */
-if (typeof browser === 'undefined') browser = chrome;
-browser.webRequest.onHeadersReceived.addListener(details => {
-	/* 組み立てに必要な情報を入手 */
-	const info_filename    = /filename="(.+)(\.\w{1,20})"/.exec(getResponseHeader(details, 'Content-Disposition'));
-	const material_id      = info_filename[1];
-	const material_ext     = info_filename[2];
-	const material_title   = sessionStorage.getItem('commons-title-'+material_id);
-	const material_creator = sessionStorage.getItem('commons-creator-'+material_id);
-	/* タイトルや作者が正常にキャッシュされていたか確認 */
-	if (String(material_title) === 'null' || String(material_creator) === 'null') {
-		return {
-			responseHeaders: details.responseHeaders,
-		};
-	}
-	/* 各種情報を組み立てて反映 */
-	const material_name = replaceSpecialChars(localStorage.getItem('file_pattern').replace('${id}', material_id).replace('${title}', material_title).replace('${creator}', material_creator));
-	setResponseHeader(details, 'Content-Disposition', 'attachment; filename="'+encodeURI(material_name)+material_ext+'"; filename*=UTF-8\'\''+encodeURI(material_name)+material_ext);
+// browser.webRequest.onHeadersReceived.addListener(details => {
+// 	/* 組み立てに必要な情報を入手 */
+// 	const info_filename    = /filename="(.+)(\.\w{1,20})"/.exec(getResponseHeader(details, 'Content-Disposition'));
+// 	const material_id      = info_filename[1];
+// 	const material_ext     = info_filename[2];
+// 	const material_title   = sessionStorage.getItem('commons-title-'+material_id);
+// 	const material_creator = sessionStorage.getItem('commons-creator-'+material_id);
+// 	/* タイトルや作者が正常にキャッシュされていたか確認 */
+// 	if (String(material_title) === 'null' || String(material_creator) === 'null') {
+// 		return {
+// 			responseHeaders: details.responseHeaders,
+// 		};
+// 	}
+// 	/* 各種情報を組み立てて反映 */
+// 	const material_name = replaceSpecialChars(localStorage.getItem('file_pattern').replace('${id}', material_id).replace('${title}', material_title).replace('${creator}', material_creator));
+// 	setResponseHeader(details, 'Content-Disposition', 'attachment; filename="'+encodeURI(material_name)+material_ext+'"; filename*=UTF-8\'\''+encodeURI(material_name)+material_ext);
 
-	return {
-		responseHeaders: details.responseHeaders,
-	};
-}, {
-	urls: ['https://deliver.commons.nicovideo.jp/download/*', 'http://deliver.commons.nicovideo.jp/download/*'],
-}, [
-	'responseHeaders',
-	'blocking'
-]);
+// 	return {
+// 		responseHeaders: details.responseHeaders,
+// 	};
+// }, {
+// 	urls: ['https://deliver.commons.nicovideo.jp/download/*', 'http://deliver.commons.nicovideo.jp/download/*'],
+// }, [
+// 	'responseHeaders',
+// 	'blocking'
+// ]);
 
 
 /* --- 拡張機能の各要素からのメッセージに反応する --- */
@@ -46,18 +52,22 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	}
 	/* オプションを送り返す */
 	if (message.content === 'get-option') {
-		sendResponse({
-			'file-pattern'  : localStorage.getItem('file_pattern'),
-			'copy-title'    : (localStorage.getItem('copy_title') === 'true'),
-			'title-pattern' : localStorage.getItem('title_pattern')
+		browser.storage.local.get(['file_pattern', 'copy_title', 'title_pattern'], options => {
+			sendResponse({
+				'file-pattern'  : options['file_pattern'],
+				'copy-title'    : options['copy_title'],
+				'title-pattern' : options['title_pattern']
+			});
 		});
 		return true;
 	}
 	/* オプションを設定する */
 	if (message.content === 'set-option') {
-		localStorage.setItem('file_pattern', message['file-pattern']);
-		localStorage.setItem('copy_title', message['copy-title']);
-		localStorage.setItem('title_pattern', message['title-pattern']);
+		browser.storage.local.set({
+			file_pattern  : message['file-pattern'],
+			copy_title    : message['copy-title'],
+			title_pattern : message['title-pattern']
+		});
 	}
 });
 
@@ -73,8 +83,8 @@ function setResponseHeader(details, key, val) {
 		}
 	}
 	details.responseHeaders.push({
-		name: key,
-		value: val,
+		name  : key,
+		value : val,
 	});
 }
 
